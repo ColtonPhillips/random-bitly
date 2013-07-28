393ac8dd7b4c50cffb3bd1eb8f567a882707d9e2
robertjanzen <robertjanzenbc@gmail.com>
2013-07-27 22:24:45 -0700

    remove unused import

--- short.py
+++ short.py
@@ -3,7 +3,6 @@ import string
 import random
 from urllib2 import urlopen
 from urllib2 import HTTPError
-from BeautifulSoup import BeautifulSoup
 
 def id_generator(size=6, chars=string.ascii_letters + string.digits):
 	return 'http://bit.ly/'+''.join(random.choice(chars) for x in range(size))
