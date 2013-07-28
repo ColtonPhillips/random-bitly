e8c5e64d6ac297c0915af78c94d8c581f02ee067
Lee Gauthier <lgauthie@uvic.ca>
2013-07-27 23:57:56 -0700

    Better error handling of title

--- twitterbot.py
+++ twitterbot.py
@@ -17,12 +17,10 @@ def main():
     url = random_bitly()
     response = urlopen(url).read()
     soup = BeautifulSoup(response)
-    while True:
-        try:
-            title = soup.title.getText()[:100]
-            break
-        except NoneType:
-            title = 'Whoops'
+
+    title = "I don't know..."
+    if soup.get('title'):
+        title = soup.title.getText()[:100]
 
     if randrange(1,3) == 1:
         bot.update_status('{title} - {url}'.format(title=title, url=url))
