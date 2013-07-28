f762b7131acf159e6998f5e7e38daa2298fc8c54
Lee Gauthier <lgauthie@uvic.ca>
2013-07-27 22:22:08 -0700

    Complete Rewrite - lol

--- short.py
+++ short.py
@@ -3,18 +3,18 @@ import string
 import random
 from urllib2 import urlopen
 from urllib2 import HTTPError
+from BeautifulSoup import BeautifulSoup
 
 def id_generator(size=6, chars=string.ascii_letters + string.digits):
 	return 'http://bit.ly/'+''.join(random.choice(chars) for x in range(size))
 
-def random_url():
-	while 1:
-		try:
-			url = id_generator()
-			urlopen(url).code
-			break
-		except HTTPError:
-			pass
-
-	return url
+def random_bitly():
+    while True:
+        try:
+            url = id_generator()
+            urlopen(url)
+            break
+        except HTTPError:
+            pass
+    return  url
 
--- twitterbot.py
+++ twitterbot.py
@@ -1,13 +1,28 @@
+from random import randrange
+
 # Twitter Bitly Bot
-from short import random_url
 import tweepy
+
+from short import random_bitly
 
 consumer_key=''
 consumer_secret=''
 access_token_key=''
 access_token_secret=''
 
-auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
-auth.set_access_token(access_token_key,access_token_secret)
-bot = tweepy.API(auth)
-bot.update_status(random_url())
+def main():
+    auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+    auth.set_access_token(access_token_key,access_token_secret)
+    bot = tweepy.API(auth)
+    url = random_bitly()
+    response = urlopen(url).read()
+    soup = BeautifulSoup(response)
+    title = soup.title.getText()[:100]
+
+    if randrang(1,6) == 1:
+        bot.update_status(random_url('{title} - {url}'.format(title=title, url=url)))
+    else:
+        bot.update_status(random_url("{title} - You'll never know".format(title=title)))
+
+if __name__=='__main__':
+    main()
