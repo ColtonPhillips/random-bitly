7240bf37b803661700bb2b43fbe6f3506f6cd1b5
robertjanzen <robertjanzenbc@gmail.com>
2013-07-29 08:44:13 -0700

    Add comments and change import short to bitlify

--- twitterbot.py
+++ twitterbot.py
@@ -1,23 +1,29 @@
-#Twitter Bitly Bot
-from short import random_bitly
+# Twitter Bitly Bot
+from bitlify import random_bitly
 import tweepy
 from urllib import urlopen
 from BeautifulSoup import BeautifulSoup
 from random import randrange
 
+# Twitter developer account authentication credentials
 consumer_key=''
 consumer_secret=''
 access_token_key=''
 access_token_secret=''
 
 def main():
+    
+    # Sign in to Twitter
     auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
     auth.set_access_token(access_token_key,access_token_secret)
     bot = tweepy.API(auth)
+    
+    # Get URL and find page title
     url = random_bitly()
     response = urlopen(url).read()
     soup = BeautifulSoup(response)
 
+    # Beat the Twitter spam filter
     if soup.find('title'):
         title = soup.find('title').getText()[:100]
         if randrange(1,3) == 1:
