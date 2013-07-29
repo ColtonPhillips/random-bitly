37037cfcfc49b2cd01ab917955001c129ac56440
robertjanzen <robertjanzenbc@gmail.com>
2013-07-29 08:41:28 -0700

    Add comments and rename to bitlify

--- bitlify.py
+++ bitlify.py
@@ -5,16 +5,17 @@ from urllib2 import urlopen
 from urllib2 import HTTPError
 from urllib2 import URLError
 
+# Create bitly address
 def id_generator(size=6, chars=string.ascii_letters + string.digits):
 	return 'http://bit.ly/'+''.join(random.choice(chars) for x in range(size))
 
+# Find valid bitly address
 def random_bitly():
     while True:
         try:
             url = id_generator()
             urlopen(url)
             break
-        # Round brackets need because python bug... remember that Bert.
         except (HTTPError, URLError):
             pass
     return  url
